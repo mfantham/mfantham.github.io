41d21d153898102a09aa08b9361e025936cc8c7b
Marcus <m@fnt.hm>
2023-10-23 23:08:13 -0700

    rerouting which is supported by github pages

--- src/index.js
+++ src/index.js
@@ -2,12 +2,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import { checkForRedirect } from "./rerouting";
 
-const redirecting = checkForRedirect();
-
-if (!redirecting) {
-  const rootElement = document.getElementById("root");
-  const root = createRoot(rootElement);
-  root.render(<App />);
-}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(<App />);
